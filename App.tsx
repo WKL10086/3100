b98cb5d4392460d9476a008feadc8aa2479f0c38
markm812 <53172013+markm812@users.noreply.github.com>
2021-04-28 20:11:27 +0800

    Updated with comments

--- App.tsx
+++ App.tsx
@@ -30,9 +30,20 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [logged, setLogged] = React.useState(0);
+  
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // the temp local URL for connecting to the DataBase
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   var loginURL = "http://10.0.2.2:3000/users/";
   var tempt_URL = "http://10.0.2.2:3000/";
 
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // this function consists of two parts
+  // 1. the const address where it fetch contents of specified URL
+  // 2. the function calling for updating the const address
+  // this is used for checking login details 
+  // in order to update the application's login status
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   function updateLoginState(URL) {
     const address = fetch(URL)
       .then((response) => response.json())
@@ -47,7 +58,15 @@ export default function App() {
     };
     printAddress();
   }
-
+  
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // this function is called by the resigter operation
+  // with given ID and PW the login URL will be generated
+  // the application will attempt to connect via this URL
+  // to extract info in the DB
+  // this part is also related to backend routing
+  // see userRoute.js for more details
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   function triggerSignUp(UN,PW) {
     const t_url = loginURL + "reg/"+UN + "/" + PW;
     const address = fetch(t_url)
@@ -62,12 +81,27 @@ export default function App() {
     };
     printAddress();
   }
+  
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // in the first run of the application, we initialize
+  // the login status to false and entered userName and userToken to null
+  // userToken is the identifier for the application to know 
+  // which user is the current one
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   const initialLogin = {
     isLoading: true,
     userName: null,
     userToken: null,
   };
 
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // this is the manager function for login operations
+  // switching 4 cases: 
+  // First: first time opening the application
+  // Login: when users try to login with ID and PW
+  // Logout: user operation asking the application to logout
+  // Register: users try to sign up with ID and PW
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   const loginManager = (prevState, action) => {
     switch (action.type) {
       case "FIRST":
@@ -99,10 +133,26 @@ export default function App() {
         };
     }
   };
+  
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // function for setting Login/Logout status
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   function setLogOut() {
     setLogged(0);
   }
+  
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // const terms for telling the application to use loginManger as the login handling function
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   const [loginState, dispatch] = React.useReducer(loginManager, initialLogin);
+  
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // the authContext is the authorization part for the application, mainly handling login authorization
+  // consists of signIn(), signOut(), signUp(), resetPW()
+  // these are contained inside the React.memorization so they can be passed to different .js file
+  // signIn: calling updateLoginState() -> return true -> set userToken; else -> alert users
+  // signOut: calling setLogOut() -> set loginState -> false
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   const authContext = React.useMemo(
     () => ({
       signIn: async (userName, password) => {
@@ -111,8 +161,6 @@ export default function App() {
         userToken = null;
         if (userName == "admin" && password == "debug") passed = true;
         tempt_URL = loginURL + userName + "/" + password;
-        //console.log("Login details are: ", userName, " and ", password);
-        //console.log("The value of 2nd logged is ", logged);
         updateLoginState(tempt_URL);
         if (passed || logged) {
           userToken = userName;
@@ -136,16 +184,18 @@ export default function App() {
         }
         dispatch({ type: "LOGOUT" });
       },
-
+      
+      
+      // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
       // This function will be called at register,
       // it returns username and password demanded by the user
       // insert these two value with proper schema/relation into correct table in the DB
-
+      // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
       signUp: (userName, password) => {
         alert("Closed beta period, your sign up will be handled soon.");
         let userToken;
         userToken = null;
-        //triggerSignUp(userName,password);
+        // triggerSignUp(userName,password);
         dispatch({ type: "REGISTER", id: userName, token: userToken });
       },
       resetPW: () => {
@@ -157,9 +207,10 @@ export default function App() {
     []
   );
 
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   // This userEffect will try to login automatically
   // with previous saved data after opening the app for 1 seconds
-
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   useEffect(() => {
     setTimeout(async () => {
       let userToken;
@@ -213,7 +264,10 @@ export default function App() {
           ) : (
             // Note:
             // This part below is the login screen
-
+            // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+            // if the useEffect doesn't pass -> previous saved data fails the login -> old/invalid/wrong
+            // asking users to input ID and PW again, also showing other functions like signUp/forgot PW
+            // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
             <AuthStack.Navigator initialRouteName="Login">
               <AuthStack.Screen name="Login" component={LoginHome} />
               <AuthStack.Screen name="Reset" component={Reset} />
